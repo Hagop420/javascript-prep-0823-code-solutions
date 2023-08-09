7050bfcd2aec605326c62cd8684f6fcbbb169d35
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-08-09 03:22:00 +0000

    Completed prep-jascript-objects

--- prep-javascript-objects/main.js
+++ prep-javascript-objects/main.js
@@ -4,15 +4,16 @@ const person = {
   hobby: 'Coding',
 };
 
-person.previousJob = 'Asssistant mechanic';
 person.job = 'Coder';
 
 console.log(person);
 
 console.log(`The person's name is ${person.firstName} ${person.lastName}`);
 
-console.log(`The person' current job is ${person.job}`);
+console.log(`The person' current job is: ${person.job}`);
 
-console.log(`The person'previous job is ${person.previousJob}`);
+person.previousJob = 'Asssistant mechanic';
+
+console.log(`The person'previous job is: ${person.previousJob}`);
 
 console.log('The complete person object:', person);
