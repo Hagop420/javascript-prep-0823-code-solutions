961033a6cf20304a48afc58194c5aefed9832ff8
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-08-09 03:50:42 +0000

    done

---
+++ prep-javascript-operators-and-expressions/main.js
@@ -0,0 +1,34 @@
+// 1
+const num = 28;
+console.log(`value of product: ${num}`);
+console.log(`type of product: ${typeof num}`);
+
+// 2
+const remainingAM = 5.5;
+console.log(`value of amountRemaining: ${remainingAM}`);
+console.log(`typeof amountRemaining: ${typeof remainingAM}`);
+
+// 3
+const grade = 90;
+console.log(`value of grade: ${grade}`);
+console.log(`typeof grade: ${typeof grade}`);
+
+// 4
+const fullName = 'Ron Burgundy';
+console.log(`value of fullName: ${fullName}`);
+console.log(`typeof fullName: ${typeof fullName}`);
+
+// 5
+const isAcidic = false;
+console.log(`value of isAcidic: ${isAcidic}`);
+console.log(`typeof isAcidic: ${typeof isAcidic}`);
+
+// 6
+const isSparta = 90;
+console.log(`value of isSparta: ${isSparta}`);
+console.log(`typeof isSparta: ${typeof isSparta}`);
+
+// 7
+const nickname = 'Ron Burgundy is numero uno';
+console.log(`value of nickname: ${nickname}`);
+console.log(`typeof nickname: ${typeof nickname}`);
