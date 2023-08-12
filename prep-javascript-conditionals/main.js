ab8e9c630aa07c7c2bac327b804fe6bd6f9a5d6c
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-08-12 18:57:57 +0000

    Completed prep-javascript-conditionals -Jack Almadjian

--- prep-javascript-conditionals/main.js
+++ prep-javascript-conditionals/main.js
@@ -1,1 +1,67 @@
 /* Your JS goes in this file */
+
+const isAdult = (age) => {
+  if (age < 18) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+console.log(isAdult(121));
+
+const didStudentPass = (score) => {
+  if (score >= 70) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+console.log(didStudentPass(111));
+
+const gradeCalculator = (score) => {
+  if (score < 60) {
+    return 'F';
+  } else if (score < 70) {
+    return 'D';
+  } else if (score < 80) {
+    return 'C';
+  } else if (score < 90) {
+    return 'B';
+  } else if (score < 100) {
+    return 'A ';
+  } else {
+    return 'A++';
+  }
+};
+
+console.log(gradeCalculator(100));
+
+const seasonMessenger = (season) => {
+  if (season === 'summer') {
+    return "it's hot today";
+  } else if (season === 'spring') {
+    return 'the flowers are blooming';
+  } else if (season === 'autumn') {
+    return 'the leaves are changing colors';
+  } else if (season === 'winter') {
+    return "it's cold today";
+  } else {
+    return 'please enter a valid season';
+  }
+};
+
+console.log(seasonMessenger('spring'));
+
+const dayDetector = (dayOfTheWeek) => {
+  if (dayOfTheWeek === 'Saturday' || dayOfTheWeek === 'Sunday') {
+    return 'have a good weekend!';
+  } else {
+    return 'Its a weekday';
+  }
+};
+
+console.log(
+  dayDetector(new Date().toLocaleString('default', { weekday: 'long' }))
+);
