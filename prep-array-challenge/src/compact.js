4a44d8a4688e76d57494e3185a0006955d0846e5
Hagop Almadjian <hagop1isaboss@hotmail.com>
2023-08-18 00:57:28 +0000

    Completed prep-array-challenge

--- prep-array-challenge/src/compact.js
+++ prep-array-challenge/src/compact.js
@@ -1,1 +1,17 @@
 /* exported compact */
+const compact = (array) => {
+  const result = [];
+
+  for (const element of array) {
+    if (element) {
+      result.push(element);
+    }
+  }
+
+  return result;
+};
+
+console.log(compact(['foo', 'bar', 'baz']));
+console.log(compact([1, 2, 3, 4, 5]));
+console.log(compact([false, true, false, true]));
+console.log(compact([]));
--- prep-array-challenge/src/tail.js
+++ prep-array-challenge/src/tail.js
@@ -1,1 +1,13 @@
 /* exported tail */
+const tail = (array) => {
+  const newArr = [];
+  for (let i = 1; i < array.length; i++) {
+    newArr.push((array[i - 1] = array[i]));
+  }
+  return newArr;
+};
+
+console.log(tail(['foo', 'bar', 'baz']));
+console.log(tail([1, 2, 3, 4, 5]));
+console.log(tail([false, true, false, true]));
+console.log(tail([]));
